7ef93850cd7084052a329d8191bb55a84190dde6
Buldakov <abuldakov10@gmail.com>
2022-10-12 16:00:58 +0400

    Add dynamic slider with api request on load

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,119 @@
 document.addEventListener('DOMContentLoaded', () => {
-  /*** Slider ***/
-  const slideCaptions = document.querySelector('.main-slider__captions .container');
+  const sliderWrapper = document.querySelector('.swiper-wrapper');
+  const titleCaptions = document.querySelector(
+    '.main-slider__captions .main-slider__title'
+  );
+  const subTitleCaptions = document.querySelector(
+    '.main-slider__captions .main-slider__subtitle'
+  );
+  const likeCaptions = document.querySelector(
+    '.main-slider__captions .like__counter'
+  );
+  const api = 'https://private-57b4e3-grchhtml.apiary-mock.com/slides';
 
-  const mainSlider = new Swiper('.js-slider', {
-    watchOverflow: true,
-    allowTouchMove: false,
-    speed: 1200,
-    effect: 'creative',
-    creativeEffect: {
-      prev: {
-        translate: ['-55%', 0, 0],
-      },
-      next: {
-        translate: ['100%', 0, 0],
-      },
-    },
+  // API request
+  const fetchSlides = async (offset, limit) => {
+    const data = await fetch(`${api}?offset=${offset}&limit=${limit}`);
+    return await data.json();
+  };
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  /*** Fetch and init slider on load ***/
+  fetchSlides(0, 3).then((slides) => {
+    const createSlideItem = slides.data.map(
+      ({ id, title, desc, likeCnt, imgUrl }) => {
+        const li = document.createElement('div');
 
-    on: {
-      slideChangeTransitionStart: function () {
-        const slideTitle = this.slides[this.activeIndex].firstElementChild.getAttribute('data-title');
-        const slideSubTitle = this.slides[this.activeIndex].firstElementChild.getAttribute('data-subtitle');
+        li.classList.add('swiper-slide');
+        li.innerHTML = `<div class="main-slider__item" data-id="${id}" data-title="${title}" data-subtitle="${desc}" data-like-count="${likeCnt}" style="background-image: url('${imgUrl}')">
+                            <div class="container">
+                              <div class="main-slider__title" style="display: -webkit-box; -webkit-line-clamp: 2;" title="${title}">${title}</div>
+                            </div>
+                        </div>`;
 
-        slideCaptions.innerHTML = `<div class="main-slider__title main-slider__title_captured" style="display: -webkit-box; -webkit-line-clamp: 2;" title="${slideTitle}">
-                                      <span>${slideTitle}</span>
-                                   </div>
-                                   <div class="main-slider__subtitle" style="display: -webkit-box; -webkit-line-clamp: 4;">${slideSubTitle}</div>`;
-        }
-    }
+        return li;
+      }
+    );
+
+    createSlideItem.forEach((item) => {
+      sliderWrapper.append(item);
+    });
+
+    // Init slider
+    mainSlider();
   });
 
+  /*** Slider init function ***/
+  function mainSlider() {
+    const swiper = new Swiper('.js-slider', {
+      watchOverflow: true,
+      allowTouchMove: false,
+      speed: 1200,
+      effect: 'creative',
+      creativeEffect: {
+        prev: {
+          translate: ['-55%', 0, 0],
+        },
+        next: {
+          translate: ['100%', 0, 0],
+        },
+      },
 
-  const firstSlideTitle = mainSlider.slides[mainSlider.activeIndex].firstElementChild.getAttribute('data-title');
-  const firstSlideSubTitle = mainSlider.slides[mainSlider.activeIndex].firstElementChild.getAttribute('data-subtitle');
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
-  slideCaptions.innerHTML = `<div class="main-slider__title main-slider__title_captured" style="display: -webkit-box; -webkit-line-clamp: 2;" title="${firstSlideTitle}">
-                                <span>${firstSlideTitle}</span>
-                             </div>
-                             <div class="main-slider__subtitle" style="display: -webkit-box; -webkit-line-clamp: 4;">${firstSlideSubTitle}</div>`;
+      on: {
+        slideChangeTransitionStart: function () {
+          const slideTitle =
+            this.slides[this.activeIndex].firstElementChild.getAttribute(
+              'data-title'
+            );
+          const slideSubTitle =
+            this.slides[this.activeIndex].firstElementChild.getAttribute(
+              'data-subtitle'
+            );
+          const slideLikeCount =
+            this.slides[this.activeIndex].firstElementChild.getAttribute(
+              'data-like-count'
+            );
+
+          titleCaptions.innerHTML = `<span>${slideTitle}</span>`;
+          subTitleCaptions.innerHTML = slideSubTitle;
+          likeCaptions.innerHTML = slideLikeCount;
+        },
+      },
+    });
+
+    // Set caption for first slider on load
+    const firstSlideTitle =
+      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
+        'data-title'
+      );
+
+    const firstSlideSubTitle =
+      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
+        'data-subtitle'
+      );
+
+    const firstSlideLikeCount =
+      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
+        'data-like-count'
+      );
+
+    titleCaptions.innerHTML = `<span>${firstSlideTitle}</span>`;
+    subTitleCaptions.innerHTML = firstSlideSubTitle;
+    likeCaptions.innerHTML = firstSlideLikeCount;
+  }
+
+  /*** Likes ***/
+  // const likeBtn = document.querySelector('.js-like');
+  // const popup = document.querySelector('.popup');
+  // const body = document.querySelector('body');
+
+  // likeBtn.addEventListener('click', () => {
+  //   popup.classList.remove('hidden');
+  // body.classList.add('no-scroll');
+  // });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 /*** Variables ***/
 const gulp = require('gulp'),
-      postcss = require('gulp-postcss'),
-      sass = require('gulp-sass'),
-      mqpacker = require('css-mqpacker'),
-      csso = require('gulp-csso'),
-      autoprefixer = require('autoprefixer'),
-      unprefix = require('postcss-unprefix'),
-      fileinclude = require('gulp-file-include'),
-      webpHtml = require('gulp-webp-html'),
-      imagemin = require('gulp-imagemin'),
-      imagewebp = require('gulp-webp'),
-      uglifyJS = require('gulp-uglify'),
-      babel = require('gulp-babel'),
-      sourcemaps = require('gulp-sourcemaps'),
-      concat = require('gulp-concat'),
-      clean = require('gulp-clean'),
-      browserSync = require('browser-sync');
+  babel = require('gulp-babel'),
+  postcss = require('gulp-postcss'),
+  sass = require('gulp-sass'),
+  mqpacker = require('css-mqpacker'),
+  csso = require('gulp-csso'),
+  autoprefixer = require('autoprefixer'),
+  unprefix = require('postcss-unprefix'),
+  fileinclude = require('gulp-file-include'),
+  webpHtml = require('gulp-webp-html'),
+  imagemin = require('gulp-imagemin'),
+  imagewebp = require('gulp-webp'),
+  uglifyJS = require('gulp-uglify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  clean = require('gulp-clean'),
+  browserSync = require('browser-sync');
 
 /*** Path ***/
 let path = {
@@ -24,89 +24,117 @@ let path = {
     scss: 'app/scss/**/main.scss',
     js: ['app/js/swiper-bundle.min.js', 'app/js/main.js'],
     img: 'app/img/**/*.*',
-    fonts_woff: ['app/fonts/**/*.*']
+    fonts_woff: ['app/fonts/**/*.*'],
   },
   watch: {
     html: 'app/templates_html/**/*.html',
     scss: 'app/scss/**/*.scss',
     js: 'app/js/*.js',
-    img: 'app/img/**/*.*'
+    img: 'app/img/**/*.*',
   },
   build: {
     html: 'dest/*.html',
     css: 'dest/css/**/*.css',
     js: 'dest/js/*.js',
     img: 'dest/img/**/*.*',
-    fonts: 'dest/fonts/**/*.*'
-  }
-}
+    fonts: 'dest/fonts/**/*.*',
+  },
+};
 
 /*** Dev tasks ***/
 // HTML task
 gulp.task('html', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      prefix: '@',
-      basepath: '@file'
-    }))
+  return gulp
+    .src(path.src.html)
+    .pipe(
+      fileinclude({
+        prefix: '@',
+        basepath: '@file',
+      })
+    )
     .pipe(webpHtml())
     .pipe(gulp.dest('dest'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // SCSS task
 gulp.task('sass', function () {
-  return gulp.src(path.src.scss)
+  return gulp
+    .src(path.src.scss)
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(postcss([
-        unprefix(),
-        autoprefixer(),
-        mqpacker({sort: true})
-    ]))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([unprefix(), autoprefixer(), mqpacker({ sort: true })]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dest/css'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // JS task
 gulp.task('js', function () {
-  return gulp.src(path.src.js)
+  return gulp
+    .src(path.src.js)
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
-    .pipe(babel({presets: [["@babel/preset-env"]]}))
+    .pipe(
+      babel({
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              // useBuiltIns: false,
+            },
+          ],
+        ],
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              // absoluteRuntime: false,
+              // corejs: false,
+              helpers: false,
+              // regenerator: true,
+              // useESModules: false,
+            },
+          ],
+        ],
+      })
+    )
     .pipe(gulp.dest('dest/js'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // IMG task
 gulp.task('images', function () {
-  return gulp.src(path.src.img)
-    .pipe(imagemin({
-      progressive: true,
-      optimizationLevel: 3
-    }))
+  return gulp
+    .src(path.src.img)
+    .pipe(
+      imagemin({
+        progressive: true,
+        optimizationLevel: 3,
+      })
+    )
     .pipe(gulp.dest('dest/img'))
-    .pipe(imagewebp({
-      quality: 70
-    }))
+    .pipe(
+      imagewebp({
+        quality: 70,
+      })
+    )
     .pipe(gulp.dest('dest/img'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Fonts
 gulp.task('woff2', function () {
-  return gulp.src(path.src.fonts_woff)
-    .pipe(gulp.dest('dest/fonts'))
+  return gulp.src(path.src.fonts_woff).pipe(gulp.dest('dest/fonts'));
 });
 
 // Browser Sync task
 gulp.task('browser-sync', function () {
   browserSync({
     server: {
-      baseDir: 'dest'
+      baseDir: 'dest',
     },
-    notify: false
+    notify: false,
   });
 });
 
@@ -127,45 +155,52 @@ gulp.task('watch-changes', gulp.parallel('browser-sync', 'watch'));
 
 gulp.task('default', gulp.series('compile', 'watch-changes'));
 
-
 /*** Build tasks ***/
 // Clean build directory task
 gulp.task('build-clean', function () {
-  return gulp.src('build')
-    .pipe(clean()) // Clean build directory
+  return gulp.src('build').pipe(clean()); // Clean build directory
 });
 
 // Task for moving all HTML files from dest to build directory
-gulp.task ('build-html', function () {
-  return gulp.src(path.build.html)
-    .pipe(gulp.dest('build'))
+gulp.task('build-html', function () {
+  return gulp.src(path.build.html).pipe(gulp.dest('build'));
 });
 
 // Task for moving all CSS files from dest to build directory
-gulp.task ('build-css', function () {
-  return gulp.src(path.build.css)
+gulp.task('build-css', function () {
+  return gulp
+    .src(path.build.css)
     .pipe(csso()) // Minify CSS
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('build/css'));
 });
 
 // Task for moving all JS files from dest to build directory
-gulp.task ('build-js', function () {
-  return gulp.src(path.build.js)
+gulp.task('build-js', function () {
+  return gulp
+    .src(path.build.js)
     .pipe(uglifyJS()) // Minify JS
-    .pipe(gulp.dest('build/js'))
+    .pipe(gulp.dest('build/js'));
 });
 
 // Task for moving all images from dest to build directory
-gulp.task ('build-images', function () {
-  return gulp.src(path.build.img)
-    .pipe(gulp.dest('build/img'))
+gulp.task('build-images', function () {
+  return gulp.src(path.build.img).pipe(gulp.dest('build/img'));
 });
 
 // Task for moving all fonts from dest to build directory
-gulp.task ('build-fonts', function () {
-  return gulp.src(path.build.fonts)
-    .pipe(gulp.dest('build/fonts'))
+gulp.task('build-fonts', function () {
+  return gulp.src(path.build.fonts).pipe(gulp.dest('build/fonts'));
 });
 
 // Run build
-gulp.task ('build', gulp.series('build-clean', 'build-html', 'build-css', 'build-js', 'build-images', 'build-fonts'));
+gulp.task(
+  'build',
+  gulp.series(
+    'build-clean',
+    'build-html',
+    'build-css',
+    'build-js',
+    'build-images',
+    'build-fonts'
+  )
+);
