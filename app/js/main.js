d08f7bebd2d46239373abe33f168475e8d455c8a
Buldakov <abuldakov10@gmail.com>
2022-10-12 20:42:49 +0400

    Fetch additional slides on the next btn click, slider fixes

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
     '.main-slider__captions .like__counter'
   );
   const api = 'https://private-57b4e3-grchhtml.apiary-mock.com/slides';
+  let offset = 0;
+  let limit = 3;
 
   // API request
   const fetchSlides = async (offset, limit) => {
@@ -18,94 +20,141 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   /*** Fetch and init slider on load ***/
-  fetchSlides(0, 3).then((slides) => {
-    const createSlideItem = slides.data.map(
-      ({ id, title, desc, likeCnt, imgUrl }) => {
-        const li = document.createElement('div');
+  fetchSlides(offset, limit).then(({ data }) => {
+    const createSlideItem = data.map(({ id, title, desc, likeCnt, imgUrl }) => {
+      const li = document.createElement('div');
 
-        li.classList.add('swiper-slide');
-        li.innerHTML = `<div class="main-slider__item" data-id="${id}" data-title="${title}" data-subtitle="${desc}" data-like-count="${likeCnt}" style="background-image: url('${imgUrl}')">
+      li.classList.add('swiper-slide');
+      li.innerHTML = `<div class="main-slider__item" data-id="${id}" data-title="${title}" data-subtitle="${desc}" data-like-count="${likeCnt}" style="background-image: url('${imgUrl}')">
                             <div class="container">
                               <div class="main-slider__title" style="display: -webkit-box; -webkit-line-clamp: 2;" title="${title}">${title}</div>
                             </div>
                         </div>`;
 
-        return li;
-      }
-    );
+      return li;
+    });
 
     createSlideItem.forEach((item) => {
       sliderWrapper.append(item);
     });
 
     // Init slider
-    mainSlider();
-  });
-
-  /*** Slider init function ***/
-  function mainSlider() {
-    const swiper = new Swiper('.js-slider', {
-      watchOverflow: true,
-      allowTouchMove: false,
-      speed: 1200,
-      effect: 'creative',
-      creativeEffect: {
-        prev: {
-          translate: ['-55%', 0, 0],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
+    new Swiper('.js-slider', {
       on: {
-        slideChangeTransitionStart: function () {
-          const slideTitle =
+        init: function () {
+          // Set caption for first slider on load
+          const firstSlideTitle =
             this.slides[this.activeIndex].firstElementChild.getAttribute(
               'data-title'
             );
-          const slideSubTitle =
+
+          const firstSlideSubTitle =
             this.slides[this.activeIndex].firstElementChild.getAttribute(
               'data-subtitle'
             );
-          const slideLikeCount =
+
+          const firstSlideLikeCount =
             this.slides[this.activeIndex].firstElementChild.getAttribute(
               'data-like-count'
             );
 
-          titleCaptions.innerHTML = `<span>${slideTitle}</span>`;
-          subTitleCaptions.innerHTML = slideSubTitle;
-          likeCaptions.innerHTML = slideLikeCount;
+          titleCaptions.innerHTML = `<span>${firstSlideTitle}</span>`;
+          titleCaptions.setAttribute('title', `${firstSlideTitle}`);
+          subTitleCaptions.innerHTML = firstSlideSubTitle;
+          likeCaptions.innerHTML = firstSlideLikeCount;
         },
       },
     });
 
-    // Set caption for first slider on load
-    const firstSlideTitle =
-      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
-        'data-title'
-      );
+    // Increase counter slides
+    offset = offset + 3;
+  });
 
-    const firstSlideSubTitle =
-      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
-        'data-subtitle'
-      );
+  /*** Append slides ***/
+  const lastSliderBtn = document.querySelector('.swiper-button-next');
 
-    const firstSlideLikeCount =
-      swiper.slides[swiper.activeIndex].firstElementChild.getAttribute(
-        'data-like-count'
-      );
+  lastSliderBtn.addEventListener('click', function () {
+    const isLastBtn = this.classList.contains('swiper-button-disabled');
 
-    titleCaptions.innerHTML = `<span>${firstSlideTitle}</span>`;
-    subTitleCaptions.innerHTML = firstSlideSubTitle;
-    likeCaptions.innerHTML = firstSlideLikeCount;
+    if (isLastBtn) {
+      fetchNextSlides();
+    }
+  });
+
+  /*** Fetch next 3 slides function ***/
+  function fetchNextSlides() {
+    fetchSlides(offset, limit).then(({ countAll, data }) => {
+      if (offset <= countAll) {
+        const createSlideItem = data.map(
+          ({ id, title, desc, likeCnt, imgUrl }) => {
+            const li = document.createElement('div');
+
+            li.classList.add('swiper-slide');
+            li.innerHTML = `<div class="main-slider__item" data-id="${id}" data-title="${title}" data-subtitle="${desc}" data-like-count="${likeCnt}" style="background-image: url('${imgUrl}')">
+                            <div class="container">
+                              <div class="main-slider__title" style="display: -webkit-box; -webkit-line-clamp: 2;" title="${title}">${title}</div>
+                            </div>
+                        </div>`;
+
+            return li;
+          }
+        );
+
+        createSlideItem.forEach((item) => {
+          swiper.appendSlide(item);
+        });
+
+        swiper.slideNext();
+
+        // Increase counter slides
+        offset = offset + 3;
+      }
+    });
   }
+
+  /*** Slider init ***/
+  const swiper = new Swiper('.js-slider', {
+    watchOverflow: true,
+    allowTouchMove: false,
+    speed: 1200,
+    effect: 'creative',
+    creativeEffect: {
+      prev: {
+        translate: ['-55%', 0, -1],
+      },
+      next: {
+        translate: ['100%', 0, 0],
+      },
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    on: {
+      slideChangeTransitionStart: function () {
+        const slideTitle =
+          this.slides[this.activeIndex].firstElementChild.getAttribute(
+            'data-title'
+          );
+        const slideSubTitle =
+          this.slides[this.activeIndex].firstElementChild.getAttribute(
+            'data-subtitle'
+          );
+        const slideLikeCount =
+          this.slides[this.activeIndex].firstElementChild.getAttribute(
+            'data-like-count'
+          );
+
+        titleCaptions.innerHTML = `<span>${slideTitle}</span>`;
+        titleCaptions.setAttribute('title', `${slideTitle}`);
+        subTitleCaptions.innerHTML = slideSubTitle;
+        likeCaptions.innerHTML = slideLikeCount;
+      },
+    },
+  });
 
   /*** Likes ***/
   // const likeBtn = document.querySelector('.js-like');
